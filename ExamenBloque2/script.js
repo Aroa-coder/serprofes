44838dcb77191e10d49a855f7f4d4bba9bfc4ce1
aroamartindelarubia-coder <aroamartindelarubia@gmail.com>
2026-06-08 19:49:28 +0200

    borro un hmtl fuera de la carpeta agenciaviajes

---
+++ ExamenBloque2/script.js
@@ -0,0 +1,40 @@
+// 1. SELECCIÓN DE ELEMENTOS: Guardamos en variables los elementos del HTML
+const inputEquipo = document.getElementById('nombreEquipo');
+const inputProblema = document.getElementById('problema');
+const btnAbrirTicket = document.getElementById('btnAbrirTicket');
+const listaTickets = document.getElementById('listaTickets');
+const txtContador = document.getElementById('contador');
+
+// Variable para llevar la cuenta de los tickets abiertos
+let totalTickets = 0;
+
+// Escuchamos el evento 'click' en el botón
+btnAbrirTicket.addEventListener('click', () => {
+    
+    // Guardamos los textos que ha escrito el usuario (.trim() elimina espacios vacíos al inicio/final)
+    const equipo = inputEquipo.value.trim();
+    const problema = inputProblema.value.trim();
+
+    // 4. VALIDACIÓN: Si alguno de los dos campos está vacío...
+    if (equipo === "" || problema === "") {
+        alert("Faltan datos por rellenar."); // Salta el aviso en pantalla
+        return;                              // Ponemos 'return' para que el código se pare aquí y no sume nada
+    }
+
+    // 2. LA LÓGICA: Creamos un nuevo elemento de lista (li)
+    const nuevoLi = document.createElement('li');
+    
+    // Le metemos un div por dentro con los datos del formulario (para poder darle el diseño con la barra roja)
+    nuevoLi.innerHTML = `<div class="ticket-item"><strong>${equipo}</strong> : ${problema}</div>`;
+    
+    // Inyectamos el nuevo li dentro de la lista (ul) de nuestro HTML
+    listaTickets.appendChild(nuevoLi);
+
+    // 3. EL CEREBRO (Contador): Sumamos +1 a la variable y actualizamos el texto en la pantalla
+    totalTickets++;
+    txtContador.textContent = totalTickets;
+
+    // 5. LIMPIEZA: Vaciamos los inputs para que queden listos para la siguiente incidencia
+    inputEquipo.value = "";
+    inputProblema.value = "";
+});
